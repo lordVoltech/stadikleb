db0705f03f2bd6b0aeae4424f319bc106648ae52
lordVoltech <124055040+lordVoltech@users.noreply.github.com>
2025-06-17 15:27:20 +0000

    login ganti nama

--- login.php
+++ login.php
@@ -51,7 +51,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-indigo-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
                     </svg>
-                    <span class="ml-2 text-2xl font-bold text-indigo-900">LUXURY MOTORS</span>
+                    <span class="ml-2 text-2xl font-bold text-indigo-900">LUXURY AUTO</span>
                 </div>
             </div>
             
